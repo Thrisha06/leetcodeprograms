83186a6cf6352e4c02c70ce30e7f36d3643bc6cb
Thrisha06 <thrishamadineni@gmail.com>
2026-03-15 19:41:42 +0530

    change

---
+++ uniquecharacter.java
@@ -0,0 +1,13 @@
+public class uniquecharacter {
+    public static void main(String[] args) {
+        String s="leetcode";
+        for(int i=0;i<s.length();i++){
+            if(s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i))){
+                System.out.println((i));
+                break;
+            }
+        }
+        }
+}
+    
+
