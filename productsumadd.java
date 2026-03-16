41d9f2e4c3c1c7db77cef2800e39cdb5e2e54555
Thrisha06 <thrishamadineni@gmail.com>
2026-03-16 22:22:40 +0530

    change files

---
+++ productsumadd.java
@@ -0,0 +1,15 @@
+public class productsumadd {
+    public static void main(String[] args){
+        int n=254;
+        int product =1;
+        int sum=0;
+        while(n>0){
+            int digit=n%10;
+            product *= digit;
+            sum += digit;
+            n/=10;
+        }
+        System.out.println(product-sum);
+    }
+    
+}
