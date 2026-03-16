29abfb604875b57dff2ecaa5bf9507ce694b3f7a
Thrisha06 <thrishamadineni@gmail.com>
2026-03-16 22:25:42 +0530

    change files

--- productsumadd.java
+++ productsumadd.java
@@ -2,7 +2,7 @@ public class productsumadd {
     public static void main(String[] args){
         int n=254;
         int product =1;
-        int sum=0;
+        var sum=0;
         while(n>0){
             int digit=n%10;
             product *= digit;
