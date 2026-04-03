d78e9b288aa23861450c8dd78119bd530aea02f5
Thrisha06 <thrishamadineni@gmail.com>
2026-04-03 22:36:41 +0530

    changedfiles

---
+++ Ducknumber.java
@@ -0,0 +1,15 @@
+public class Ducknumber {
+    public static boolean main(String[] args){
+        int n=1203;
+        while(n>0){
+            int digit = n%10;
+            if(digit==0){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+}
+
+    
