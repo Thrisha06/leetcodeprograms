4fb57373d4edf1a375cb4d214e22cc2c64e09215
Thrisha06 <thrishamadineni@gmail.com>
2026-04-06 19:33:40 +0530

    filechanged

---
+++ romanToInt.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class RomanToInt {
+
+    public static int romanToInt(String s) {
+
+        HashMap<Character,Integer> map=new HashMap<>();
+
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int total=0;
+
+        for(int i=0;i<s.length();i++){
+            int curr=map.get(s.charAt(i));
+
+            if(i<s.length()-1 && curr<map.get(s.charAt(i+1)))
+                total-=curr;
+            else
+                total+=curr;
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        System.out.println(romanToInt(s));
+    }
+}
+
