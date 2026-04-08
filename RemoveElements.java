c65530fbe0db51e470924b10d9680975a4e45a20
Thrisha06 <thrishamadineni@gmail.com>
2026-04-08 21:52:02 +0530

    filechanged

---
+++ RemoveElements.java
@@ -0,0 +1,15 @@
+public class RemoveElements {
+    public static void main(String[] args){
+        int[] nums = {3,2,2,3,2,3,2};
+        int val = 3;
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        System.out.println(k);
+    }
+    
+}
